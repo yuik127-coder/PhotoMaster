7451c642406970af75d23be676ffc07ad49ab288
kato yui <yuik127@icloud.com>
2026-01-18 08:07:51 +0900

    Initial Commit

---
+++ PhotoMaster/PhotoMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoMasterApp.swift
+//  PhotoMaster
+//
+//  Created by Yui Kato on 2026/01/18.
+//
+
+import SwiftUI
+
+@main
+struct PhotoMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PhotoMasterTests/PhotoMasterTests.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoMasterTests.swift
+//  PhotoMasterTests
+//
+//  Created by Yui Kato on 2026/01/18.
+//
+
+import Testing
+@testable import PhotoMaster
+
+struct PhotoMasterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
